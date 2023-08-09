4b2ad10c57280cd0747ea554dd17d68218da8de0
elina <papadelina145@gmail.com>
2023-08-09 17:54:36 +0300

    Change title font and implement animated background

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,26 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+
+        rules: [
+
+            {
+
+                test: /\.css$/i,
+
+                use: ['style-loader', 'css-loader'],
+
+            },
+            {
+
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+                type: 'asset/resource',
+
+            },
+
+        ],
+
+    },
 };
