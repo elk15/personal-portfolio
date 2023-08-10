d99f26c59ca0234cbf6eb2db4dc80669b05848e9
elina <papadelina145@gmail.com>
2023-08-10 09:50:51 +0300

    Implement animation when hovering and when opening a project card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+const firstProjectBtn = document.querySelector('#one');
+
+firstProjectBtn.addEventListener('mouseover', () => {
+    document.querySelector('#one-card').classList.add('peak');
+});
+
+firstProjectBtn.addEventListener('mouseleave', () => {
+    document.querySelector('#one-card').classList.remove('peak');
+});
+
+firstProjectBtn.addEventListener('click', () => {
+    document.querySelector('#one-card').classList.remove('peak');
+    document.querySelector('#one-card').classList.add('move');
+});
